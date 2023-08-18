b8649e8b423e477385fe4072f03069f531c7ffee
yuanlin2008 <yuanlin2008@hotmail.com>
2023-08-18 17:11:46 +0800

    123

---
+++ Examples/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BehaviorTree.TreeState state = new BehaviorTree.TreeState();    
+        }
+    }
+}
